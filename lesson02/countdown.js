d1b0ba67db8dd4dd34d4b1eadadc71d65478e4ad
Jak Ashuraliev <ashuraliev@gmail.com>
2020-04-04 19:18:07 -0700

    Write a program called countdown.js

---
+++ lesson02/countdown.js
@@ -0,0 +1,23 @@
+// Write a program called countdown.js.
+// You should use one loop to console log a variable number, decrease it by one, 
+// and log that number, and stop after the number prints 0;
+
+for (let i = 10; i >= 0; i--) {
+  console.log(i);
+};
+
+/*
+Output
+
+10 
+9 
+8 
+7 
+6 
+5 
+4 
+3 
+2 
+1
+0
+*/
