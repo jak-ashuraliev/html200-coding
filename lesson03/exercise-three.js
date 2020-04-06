ea9952279d4dd6383eb493ca51ae246e7fa0b659
Jak Ashuraliev <ashuraliev@gmail.com>
2020-04-05 22:06:58 -0700

    Use a for loop to console log all of the elements plus the index and a comma

---
+++ lesson03/exercise-three.js
@@ -0,0 +1,7 @@
+// Create an array that has at least 5 strings
+let array = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seve', 'Eight', 'Nine'];
+
+// Use a for loop to console log all of the elements plus the index and a comma
+for (let i = 0; i < array.length; i++) {
+  console.log(i + ', ' + array[i]);
+}
