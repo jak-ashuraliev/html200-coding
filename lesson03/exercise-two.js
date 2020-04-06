4780edcf39ee57fa120224cda39f420191ba6382
Jak Ashuraliev <ashuraliev@gmail.com>
2020-04-05 20:49:32 -0700

    Sentence to an array and console.log 3rd word

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,10 @@
+// Take a sentence of at least five words and a period.
+let sentence = 'I do enjoy coding when I am free.';
+
+// Make an array from that sentence. 
+let arr = sentence.split(' ');
+
+// console.log the third word.
+console.log(arr[2]);
+
+// output -> "enjoy"
